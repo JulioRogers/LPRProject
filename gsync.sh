373b4fca62315943fa911bc3f63580d1a7c2a539
JulioRogers <62779393+JulioRogers@users.noreply.github.com>
2021-08-18 10:23:44 -0500

    Update gsync.sh

--- gsync.sh
+++ gsync.sh
@@ -1,13 +1,20 @@
+#The first argument has to be the directory created by the zip file
 lpr=$1
+#The second argument has to be the zip file
 zip=$2
+
 cp labeler.py $lpr
 cd $lpr
 python labeler.py --path _annotations.json
+
+#synchronization of files with repair in case of duplicates.
 rsync -ab --suffix ~.txt -P images/ ../images/
 rsync -ab --suffix ~.txt -P labels/ ../labels/
+
 cd ..
 rm -r $lpr
 rm $zip
+
 echo "num images: "
 ls -l images/ | wc -l
 echo "num labels: "
